233fa5113d927f642a44cad11aecd8b4f929ef00
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-12-28 17:32:30 +0300

    Add files via upload

--- integrations/wp-importer/load.php
+++ integrations/wp-importer/load.php
@@ -1,1 +1,12 @@
+<?php
+/**
+ * Loads the integration with WordPress Importer.
+ *
+ * @package Polylang
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Don't access directly.
+}
+
+PLL_Integrations::instance()->wp_importer = new PLL_WordPress_Importer();
